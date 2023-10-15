db72e820346e837a0d4e44e1b80977c5df71cf0e
Jean Robertou <j-rob@live.fr>
2023-10-15 17:14:54 +0200

    feat: disable delete button while deleting

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,6 +2,7 @@
 
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
+import { Spinner } from "@/app/components";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -14,14 +15,17 @@ const DeleteIssueButton = ({ issueId }: Props) => {
   const router = useRouter();
 
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const deleteIssue = async () => {
     try {
+      setIsDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push("/issues");
       router.refresh();
     } catch (error) {
       setError(true);
+      setIsDeleting(false);
     }
   };
 
@@ -29,7 +33,10 @@ const DeleteIssueButton = ({ issueId }: Props) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete Issue
+            {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
 
         <AlertDialog.Content>
