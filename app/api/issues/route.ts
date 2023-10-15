3fa4d00619cf0fa22ffc732d854392123f5bbc6d
Jean Robertou <j-rob@live.fr>
2023-10-15 21:33:34 +0200

    feat: secure application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 import { issueSchema } from "../../validationSchemas";
 import prisma from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
   const body = await request.json();
 
   const validation = issueSchema.safeParse(body);
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,8 @@ import { Box, Flex, Grid } from "@radix-ui/themes";
 import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import prisma from "@/prisma/client";
 
@@ -11,6 +13,8 @@ interface Props {
 }
 
 const IssueDetailsPage = async ({ params: { id } }: Props) => {
+  const session = await getServerSession(authOptions);
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id, 10) },
   });
@@ -25,12 +29,14 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
         <IssueDetails issue={issue} />
       </Box>
 
-      <Box>
-        <Flex direction={"column"} gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction={"column"} gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
