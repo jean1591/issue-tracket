bff51f8743753ea33a2fca1d46f163028c127961
Jean Robertou <j-rob@live.fr>
2023-10-20 17:59:55 +0200

    feat: enchance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import authOptions from "@/app/auth/authOptions";
 import { getServerSession } from "next-auth";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 
 export async function PATCH(
@@ -17,10 +17,22 @@ export async function PATCH(
 
   const body = await request.json();
 
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { assignedToUserId, description, title } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 });
+    }
   }
 
   const issue = await prisma.issue.findUnique({
@@ -33,7 +45,11 @@ export async function PATCH(
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: {
+      assignedToUserId,
+      title,
+      description,
+    },
   });
 
   return NextResponse.json(updatedIssue);
