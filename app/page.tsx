cc5d2acc61727c87d629f3a47b91de70914d463e
Jean Robertou <j-rob@live.fr>
2023-10-21 15:49:36 +0200

    feat build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import IssueSummary from "./IssueSummary";
 import LastestIssues from "./LastestIssues";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <LastestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  return <IssueSummary open={open} closed={closed} inProgress={inProgress} />;
 }
