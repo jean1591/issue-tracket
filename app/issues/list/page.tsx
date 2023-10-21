9573a54cd8518b087fd02da11f6cad31204a3fea
Jean Robertou <j-rob@live.fr>
2023-10-21 10:33:28 +0200

    feat: filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -9,10 +9,22 @@ import {
 } from "@radix-ui/themes";
 
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 import prisma from "@/prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
 
   return (
     <div>
